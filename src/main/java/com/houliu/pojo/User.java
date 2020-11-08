e4010d7220dea5eec9670a4b8fc02dbe4d451a45
hou6 <houliu114825@163.com>
2020-11-08 22:13:04 +0800

    test user commit

---
+++ src/main/java/com/houliu/pojo/User.java
@@ -0,0 +1,13 @@
+package com.houliu.pojo;
+
+/**
+ * @author houliu
+ * @created 2020-11-08  22:10
+ */
+public class User {
+
+    private int id;
+    private String name;
+    private String sex;
+
+}
